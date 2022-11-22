cf0a3b67a3c73a9b3f9a4fb67376988471d2b693
Rutvik Patel <rutvik.p4t3l@gmail.com>
2022-11-22 12:54:26 -0800

    add funcs for addition, subtraction and scalar product

--- vector.py
+++ vector.py
@@ -17,3 +17,15 @@ class Vector(object):
 
     def __eq__(self, v):
         return self.coordinates == v.coordinates
+
+    def __add__(self, v):
+        new_v = [c1 + c2 for c1, c2 in zip(self.coordinates, v.coordinates)]
+        return Vector(new_v)
+
+    def __sub__(self, v):
+        new_v = [c1 - c2 for c1, c2 in zip(self.coordinates, v.coordinates)]
+        return Vector(new_v)
+
+    def product_scalar(self, c):
+        new_v = [c * x for x in self.coordinates]
+        return Vector(new_v)
